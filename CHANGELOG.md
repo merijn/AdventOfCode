7dbe4c369fe6018a49f7232eb8ee1c2f34bb7dbb
Merijn Verstraaten <merijn@inconsistent.nl>
2019-12-02 16:23:21 +0100

    Day 1, puzzle 1.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for advent
+
+## 0.1.0.0 -- 2019-12-02
+
+* First version. Released on an unsuspecting world.
---
+++ Day1.hs
@@ -0,0 +1,37 @@
+module Main(main) where
+
+import Data.Foldable (foldl')
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+
+toMass :: Text -> Either String [Int]
+toMass = mapM toDecimal . T.lines
+  where
+    toDecimal :: Text -> Either String Int
+    toDecimal txt = case T.decimal txt of
+        Left t -> Left t
+        Right (t, remainder)
+            | T.null remainder -> Right t
+            | otherwise -> Left "Parse error!"
+
+computeFuel :: [Int] -> Int
+computeFuel = foldl' (+) 0 . map toFuel
+  where
+    toFuel :: Int -> Int
+    toFuel n = (n `div` 3) - 2
+
+main :: IO ()
+main = do
+    args <- getArgs
+    inputData <- case args of
+        [inputFile] -> T.readFile inputFile
+        _ -> putStrLn "No input file!" >> exitFailure
+
+    case toMass inputData of
+        Left err -> putStrLn err >> exitFailure
+        Right masses -> print $ computeFuel masses
