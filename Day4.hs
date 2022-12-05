0d48ac7989ec57e159819b6f553a0f077861129b
Merijn Verstraaten <merijn@inconsistent.nl>
2022-12-05 16:26:52 +0100

    Day 4, puzzle 1.

---
+++ Day4.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import Data.Void (Void)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+import Text.Megaparsec (Parsec, runParser, eof, some)
+import Text.Megaparsec.Char (char, eol)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Text.Megaparsec.Error (errorBundlePretty)
+
+type Parser = Parsec Void Text
+
+parseFile :: FilePath -> Parser a -> IO a
+parseFile inputFile parser = do
+    inputTxt <- T.readFile inputFile
+    case runParser parser inputFile inputTxt of
+        Left e -> putStrLn (errorBundlePretty e) >> exitFailure
+        Right r -> return r
+
+data Section = Section Int Int deriving (Show)
+
+sectionParser :: Parser Section
+sectionParser = Section <$> decimal <* char '-' <*> decimal
+
+sectionPairParser :: Parser (Section, Section)
+sectionPairParser = (,) <$> sectionParser <* char ',' <*> sectionParser <* eol
+
+fullyContains :: Section -> Section -> Bool
+fullyContains (Section l1 h1) (Section l2 h2)
+    | l1 <= l2 && h1 >= h2 = True
+    | l2 <= l1 && h2 >= h1 = True
+    | otherwise = False
+
+main :: IO ()
+main = do
+    args <- getArgs
+    inputData <- case args of
+        [inputFile] -> parseFile inputFile (some sectionPairParser <* eof)
+        _ -> hPutStrLn stderr "No input file!" >> exitFailure
+
+    putStrLn "Puzzle #1:"
+    print . length $ filter (uncurry fullyContains) inputData
